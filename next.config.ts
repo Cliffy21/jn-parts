2611931ded80b625e1db62026297f2b212704535
cliffy21 <cliffordmanase8@gmail.com>
2025-12-17 17:00:59 +0300

    next updates

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-
-  // Vercel is expecting build artifacts in `Next.js/`
-  // (e.g. `/vercel/path0/Next.js/routes-manifest.json`).
-  // Align Next.js build output directory with that expectation.
-  distDir: "Next.js",
-
   images: {
     remotePatterns: [
       {
